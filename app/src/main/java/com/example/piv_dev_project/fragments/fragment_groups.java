98d8091abae68b4d6924ca42347f4d121c026bf4
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 17:40:02 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/main'""
    This reverts commit 1351cb2e69d2e96c2fc4543d87e97c2bc2f1086a.

--- app/src/main/java/com/example/piv_dev_project/fragments/fragment_groups.java
+++ app/src/main/java/com/example/piv_dev_project/fragments/fragment_groups.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.piv_dev_project.R;
+import com.example.piv_dev_project.group.AddExistGroupDialog;
+import com.example.piv_dev_project.group.AddGroupDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class fragment_groups extends Fragment {
-
+    FloatingActionButton addNewGroupFOB;
+    FloatingActionButton addExistGroupFOB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,34 @@ public class fragment_groups extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+        View view = inflater.inflate(R.layout.fragment_groups, container, false);
+
+        addNewGroupFOB = view.findViewById(R.id.addNewGroupFloatingActionButton);
+        addExistGroupFOB = view.findViewById(R.id.addExistGroupFloatingActionButton);
+
+        addNewGroupFOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddGroupDialog();
+            }
+        });
+
+        addExistGroupFOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddExistGroupDialog();
+            }
+        });
+        return view;
+    }
+
+    private void openAddExistGroupDialog() {
+        AddExistGroupDialog addFriendDialog = new AddExistGroupDialog();
+        addFriendDialog.show(getFragmentManager(), "Додати учасника");
+    }
+
+    private void openAddGroupDialog() {
+        AddGroupDialog addFriendDialog = new AddGroupDialog();
+        addFriendDialog.show(getFragmentManager(), "Додати учасника");
     }
 }
