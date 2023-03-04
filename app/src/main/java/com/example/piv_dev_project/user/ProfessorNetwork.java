c949ffc0a2ac062e22df9c8542d966c18cbb94e7
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-03-04 16:53:56 +0200

    trash clear

--- app/src/main/java/com/example/piv_dev_project/user/ProfessorNetwork.java
+++ app/src/main/java/com/example/piv_dev_project/user/ProfessorNetwork.java
@@ -3,7 +3,8 @@ package com.example.piv_dev_project.user;
 public class ProfessorNetwork extends ProfessorClass{
     String  id;
 
-    public ProfessorNetwork(String id) {
+    public ProfessorNetwork(String name, String surname, String id) {
+        super(name, surname);
         this.id = id;
     }
 }
