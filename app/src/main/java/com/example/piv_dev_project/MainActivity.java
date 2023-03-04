ec14f27e6bea7f6e4a682283c5e37f100e1efb36
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 17:42:58 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/piv_dev_project/MainActivity.java
+++ app/src/main/java/com/example/piv_dev_project/MainActivity.java
@@ -1,22 +1,55 @@
 package com.example.piv_dev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.piv_dev_project.auth.RegisterActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     static FirebaseUser currentUser;
+    private BottomNavigationView bottomNavigationView;
+    private static NavController navCo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView);
+        navCo = navHostFragment.getNavController();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    navCo.navigate(R.id.fragment_main);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Ваші справи</font></b>"));
+
+                    return true;
+                case R.id.navigation_dashboard:
+                    navCo.navigate(R.id.fragment_groups);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Список учасників</font></b>"));
+
+                    return true;
+                case R.id.navigation_notifications:
+                    navCo.navigate(R.id.fragment_settings);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Налаштування</font></b>"));
+
+                    return true;
+
+            }
+            return false;
+        });
     }
 
     @Override
