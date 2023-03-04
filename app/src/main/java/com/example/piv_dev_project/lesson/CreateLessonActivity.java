c512e26383e45d4bbcf7a15b2c9780e22af0097b
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 15:19:06 +0100

    Add lesson activity

---
+++ app/src/main/java/com/example/piv_dev_project/lesson/CreateLessonActivity.java
@@ -0,0 +1,16 @@
+package com.example.piv_dev_project.lesson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.piv_dev_project.R;
+
+public class CreateLessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_lesson);
+    }
+}
