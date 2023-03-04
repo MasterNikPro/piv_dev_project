1905663e9dcd88e6673176ff58a5c1863834e78a
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 17:20:27 +0100

    Create navigation

--- app/src/main/java/com/example/piv_dev_project/MainActivity.java
+++ app/src/main/java/com/example/piv_dev_project/MainActivity.java
@@ -1,22 +1,56 @@
 package com.example.piv_dev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 
 import com.example.piv_dev_project.auth.RegisterActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     static FirebaseUser currentUser;
+    public static NavController navCo;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView);
+        navCo = navHostFragment.getNavController();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.calendar_item:
+                    navCo.navigate(R.id.fragment_main);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Ваші справи</font></b>"));
+
+                    return true;
+                case R.id.friends_item:
+                    navCo.navigate(R.id.fragment_groups);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Список учасників</font></b>"));
+
+                    return true;
+                case R.id.settings_item:
+                    navCo.navigate(R.id.fragment_settings);
+                    //toolbar.setTitle(Html.fromHtml("<b><font face = '' color='" + titleColor + "'>Налаштування</font></b>"));
+
+                    return true;
+
+            }
+            return false;
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/piv_dev_project/group/AddExistGroupDialog.java
@@ -0,0 +1,93 @@
+package com.example.piv_dev_project.group;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.piv_dev_project.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class AddExistGroupDialog extends AppCompatDialogFragment {
+
+    private TextInputEditText idExistGroup;
+    private String idExistGroupStr;
+    FirebaseFirestore db;
+
+    private AddFriendListener listener;
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_add_exist_group, null);
+
+        idExistGroup = view.findViewById(R.id.add_exist_group_set_namegroup_edit_text);
+
+        builder.setView(view)
+                .setNegativeButton("скасувати", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("додати", new DialogInterface.OnClickListener() {
+                    //тут инициализация имейла друга
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        idExistGroupStr = Objects.requireNonNull(idExistGroup.getText()).toString();
+                        class Lol{
+                            String friend;
+
+                            public String getFriend() {
+                                return friend;
+                            }
+                            Lol(String friend){
+                                this.friend =friend;
+                            }
+                        }
+                        Lol ab= new Lol(idExistGroupStr.toString());
+
+                        //db.collection("friends"+ MainActivity.getUser().getUser().getUid().toString()).add(ab);
+
+                        //MainActivity.navCo.navigate(R.id.friends);
+                        //listener.applyText(newFriend);
+                        //FriendsFragment.addFriend(newFriend);
+
+                    }
+                });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (AddFriendListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException();
+        }
+
+    }
+
+    public interface AddFriendListener {
+        void applyText(String friendEmail);
+    }
+
+}
