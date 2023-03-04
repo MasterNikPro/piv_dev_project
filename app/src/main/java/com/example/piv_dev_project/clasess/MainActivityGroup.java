36e8c23a685e6c7c41b409f4e35c54f0cd9c39fa
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 19:29:26 +0100

    Create Clasess package

---
+++ app/src/main/java/com/example/piv_dev_project/clasess/MainActivityGroup.java
@@ -0,0 +1,16 @@
+package com.example.piv_dev_project.clasess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.piv_dev_project.R;
+
+public class MainActivityGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_group);
+    }
+}
