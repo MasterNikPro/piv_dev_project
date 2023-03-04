38768bc56569e4e3cce72d9c87978d702a82a019
Sidroded <deinekindaniil21@gmail.com>
2023-03-04 14:25:58 +0100

    Add auth .java

--- app/src/main/java/com/example/piv_dev_project/MainActivity.java
+++ app/src/main/java/com/example/piv_dev_project/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.piv_dev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.piv_dev_project.auth.RegisterActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    static FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+            Intent intent = new Intent(this, MainActivity.class);
+        } else {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/piv_dev_project/auth/RegisterActivity.java
+++ app/src/main/java/com/example/piv_dev_project/auth/RegisterActivity.java
@@ -1,16 +1,66 @@
 package com.example.piv_dev_project.auth;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.piv_dev_project.MainActivity;
 import com.example.piv_dev_project.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "MyApp";
+
+    private EditText email_register;
+    private EditText password_register;
+    private Button button_register;
+    private FirebaseAuth mAuth;
+    ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        email_register = findViewById(R.id.editTextTextEmailAddress);
+        password_register = findViewById(R.id.editTextTextPassword);
+        button_register = findViewById(R.id.register_btn);
+
+        toolbar = getSupportActionBar();
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(androidx.appcompat.R.attr.colorAccent, typedValue, true);
+        int titleColor = typedValue.data;
+
+        toolbar.hide();
+
+        button_register.setOnClickListener(v -> {
+            if (email_register.getText().toString().isEmpty() || password_register.getText().toString().isEmpty()) {
+                Toast.makeText(RegisterActivity.this, "Заповніть всі поля", Toast.LENGTH_SHORT).show();
+            } else if (password_register.getText().toString().length() < 6) {
+                Toast.makeText(RegisterActivity.this, "Пароль має бути більше 6 символів", Toast.LENGTH_SHORT).show();
+            } else if (!email_register.getText().toString().contains("@")) {
+                Toast.makeText(RegisterActivity.this, "Введіть ваш email", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.createUserWithEmailAndPassword(email_register.getText().toString(), password_register.getText().toString())
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegisterActivity.this, "Ой, щось не так", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 }
