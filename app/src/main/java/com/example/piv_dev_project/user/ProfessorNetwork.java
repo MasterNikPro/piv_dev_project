a68636fdc616a19cc6eef07f5bb4afae09b34814
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-03-04 16:40:14 +0200

    classes

---
+++ app/src/main/java/com/example/piv_dev_project/user/ProfessorNetwork.java
@@ -0,0 +1,9 @@
+package com.example.piv_dev_project.user;
+
+public class ProfessorNetwork extends ProfessorClass{
+    String  id;
+
+    public ProfessorNetwork(String id) {
+        this.id = id;
+    }
+}
